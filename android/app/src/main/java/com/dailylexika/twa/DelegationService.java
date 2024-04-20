45eee5141908428bb7d487fbe1d07dbc3c559c04
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-04-20 21:21:15 +0800

    Android app files

---
+++ android/app/src/main/java/com/dailylexika/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.dailylexika.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
