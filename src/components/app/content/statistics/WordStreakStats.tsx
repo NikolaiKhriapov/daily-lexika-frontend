8f75f8ea4379ade148b76d5f482d6f9ca4563da8
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-06-17 14:06:46 +0800

    Darkened the green for word streak stats

--- src/components/app/content/statistics/WordStreakStats.tsx
+++ src/components/app/content/statistics/WordStreakStats.tsx
@@ -6,6 +6,7 @@ import ProgressBar from '@components/ui-common/basic/ProgressBar';
 import Skeleton from '@components/ui-common/basic/Skeleton';
 import Text from '@components/ui-common/basic/Text';
 import BadgeOrStreakCount from '@components/ui-common/complex/BadgeOrStreakCount';
+import { useColorMode } from '@chakra-ui/react';
 
 interface Props {
   word?: WordDto | undefined;
@@ -14,12 +15,16 @@ interface Props {
 export default function WordStreakStats(props: Props) {
   const { word } = props;
 
+  const { colorMode } = useColorMode();
+
+  const greenTextColor = colorMode === 'dark' ? 'green.200' : 'green';
+
   if (!word) return <Skeleton height='32px' width='auto' />;
 
   return (
     <>
       <TopContainer>
-        <Text fontSize={Size.MD} fontWeight={FontWeight.SEMIBOLD} color='green.200'>{`${word.totalStreak} / 5`}</Text>
+        <Text fontSize={Size.MD} fontWeight={FontWeight.SEMIBOLD} color={greenTextColor}>{`${word.totalStreak} / 5`}</Text>
         <BadgeOrStreakCount word={word} />
       </TopContainer>
       <ProgressBar value={(word.totalStreak / 5) * 100} colorScheme='green' />
