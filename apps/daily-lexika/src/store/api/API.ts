67339b9b76a17f528f1e68cf1127f326d2d23600
Nikolai <kolyakhryapov@gmail.com>
2024-11-21 13:47:58 +0800

    Fix

--- apps/daily-lexika/src/store/api/API.ts
+++ apps/daily-lexika/src/store/api/API.ts
@@ -4,7 +4,7 @@ import { createApi } from '@reduxjs/toolkit/query/react';
 export const API = createApi({
   reducerPath: 'api',
   baseQuery: baseQuery({
-    baseUrl: `${process.env.NEXT_PUBLIC_URL as string}/api/v1`,
+    baseUrl: process.env.NEXT_PUBLIC_URL ? `${process.env.NEXT_PUBLIC_URL as string}/api/v1` : '/api/v1',
     prepareHeaders,
   }),
   tagTypes: [
