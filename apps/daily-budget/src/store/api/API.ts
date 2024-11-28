c58d0e2a86dd0ddbb28c3dc2afb69017b3b24453
Nikolai <kolyakhryapov@gmail.com>
2024-11-29 02:42:15 +0800

    Update configuration

--- apps/daily-budget/src/store/api/API.ts
+++ apps/daily-budget/src/store/api/API.ts
@@ -4,7 +4,7 @@ import { createApi } from '@reduxjs/toolkit/query/react';
 export const API = createApi({
   reducerPath: 'api',
   baseQuery: baseQuery({
-    baseUrl: `${process.env.NEXT_PUBLIC_URL as string}/api/v1`,
+    baseUrl: `${process.env.NEXT_PUBLIC_DAILY_BUDGET_BASE_URL as string}/api/v1`,
     prepareHeaders,
   }),
   tagTypes: [
--- apps/daily-budget/src/utils/apiMethods.ts
+++ apps/daily-budget/src/utils/apiMethods.ts
@@ -1,12 +1,10 @@
-export const BASE_URL = `${process.env.NEXT_PUBLIC_URL as string}/api/v1`;
-
-const URL_AUTH = `${BASE_URL}/auth`;
+const URL_AUTH = `/auth`;
 export const ApiEndpointsAuthorization = {
   register: () => `${URL_AUTH}/register`,
   login: () => `${URL_AUTH}/login`,
 };
 
-const URL_USERS = `${BASE_URL}/users`;
+const URL_USERS = `/users`;
 export const ApiEndpointsUsers = {
   getUser: () => `${URL_USERS}/info`,
   updateUserInfo: () => `${URL_USERS}/info`,
@@ -14,14 +12,14 @@ export const ApiEndpointsUsers = {
   deleteAccount: () => `${URL_USERS}`,
 };
 
-const URL_ACCOUNTS = `${BASE_URL}/accounts`;
+const URL_ACCOUNTS = `/accounts`;
 export const ApiEndpointsAccounts = {
   getAllAccounts: () => `${URL_ACCOUNTS}`,
   createAccount: () => `${URL_ACCOUNTS}`,
   updateAccount: (accountId: number) => `${URL_ACCOUNTS}/${accountId}`,
 };
 
-const URL_EXPENSE_OPERATIONS = `${BASE_URL}/expense-operations`;
+const URL_EXPENSE_OPERATIONS = `/expense-operations`;
 export const ApiEndpointsExpenseOperations = {
   getAllExpenseOperationsByAccountId: (accountId: number) => `${URL_EXPENSE_OPERATIONS}/by-account/${accountId}`,
   createExpenseOperation: () => `${URL_EXPENSE_OPERATIONS}`,
