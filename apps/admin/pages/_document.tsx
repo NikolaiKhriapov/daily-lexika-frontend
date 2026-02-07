ef89f2516c8c90cc462029676a361c4a8a380f27
Nikolai <kolyakhryapov@gmail.com>
2026-02-07 12:30:14 +0300

    CI/CD Test

--- apps/admin/pages/_document.tsx
+++ apps/admin/pages/_document.tsx
@@ -4,49 +4,51 @@ import { ServerStyleSheet } from 'styled-components';
 import { ColorModeScript } from '@chakra-ui/react';
 import { theme } from '@library/shared/utils';
 
-export default function MyDocument() {
-  return (
-    <Html lang="en" translate="no">
-      <Head>
-        <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="theme-color" content={theme.colors.dark.background} />
-      </Head>
-      <body>
-        <ColorModeScript initialColorMode={theme.initialColorMode} />
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-MyDocument.getInitialProps = async (ctx: DocumentContext) => {
-  const sheet = new ServerStyleSheet();
-  const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-  try {
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) =>
-          sheet.collectStyles(<App {...props} />),
-      });
-
-    const initialProps = await Document.getInitialProps(ctx);
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          {sheet.getStyleElement()}
-        </>
-      ),
-    };
-  } finally {
-    sheet.seal();
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
-};
+
+  render() {
+    return (
+      <Html lang="en" translate="no">
+        <Head>
+          <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="theme-color" content={theme.colors.dark.background} />
+        </Head>
+        <body>
+          <ColorModeScript initialColorMode={theme.initialColorMode} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
